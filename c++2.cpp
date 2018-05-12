aae57ac3d4224286a0c759e09e3aacf3de57b351
Your Name <reazulislam137@gmail.com>
2018-05-12 23:57:52 +0600

    first commit

---
+++ c++2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+
+    cout << "Enter value of a and b : ";
+    cin >> a >> b;
+
+    cout<<"A : " << a << ", B : "<<b<<endl;
+
+    return 0;
+}
